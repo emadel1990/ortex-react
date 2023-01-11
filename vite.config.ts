45f142efec6d6f02d1a8209a2aeb8cdcad3b98e2
Jose Emanuel Delgado <emadevjs@gmail.com>
2023-01-11 11:26:59 -0300

    initial config v2

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react(), tsconfigPaths()],
+	build: {
+		outDir: './dist',
+		sourcemap: true,
+		emptyOutDir: true,
+		rollupOptions: {
+			output: {
+				entryFileNames: '[name].js',
+				chunkFileNames: '[name].js',
+				assetFileNames: '[name].[ext]'
+			}
+		}
+	}
+});
